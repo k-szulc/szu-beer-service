27f40e6d029094cf85ba893c545b0e45d19acf37
kamil_szulc <kamil.szulc@logintegra.com>
2022-05-21 12:16:40 +0200

    initial commit

---
+++ src/main/java/xyz/itbs/szubeerservice/SzuBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package xyz.itbs.szubeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzuBeerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SzuBeerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/itbs/szubeerservice/SzuBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.itbs.szubeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzuBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
